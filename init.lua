d9f9cfecf6e47ef32808737a48b06a4bbd9fe6c8
Sokomine <wegwerf@anarres.dyndns.org>
2015-07-21 17:25:55 +0200

    corrected dimensions of the pressure plates

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ mccompat = {}
 local mccompat_modpath = minetest.get_modpath( "mccompat");
 
 dofile(mccompat_modpath.."/gates.lua")
+dofile(mccompat_modpath.."/pressure_plates.lua")
 dofile(mccompat_modpath.."/liquids.lua")
 dofile(mccompat_modpath.."/blocklist.lua")
 
