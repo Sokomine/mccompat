d3e10e8ddb25896632f5ebc411db4323061165c8
Sokomine <wegwerf@anarres.dyndns.org>
2015-07-21 16:57:10 +0200

    moved liquid definitions to extra file

--- init.lua
+++ init.lua
@@ -5,5 +5,6 @@ mccompat = {}
 local mccompat_modpath = minetest.get_modpath( "mccompat");
 
 dofile(mccompat_modpath.."/gates.lua")
+dofile(mccompat_modpath.."/liquids.lua")
 dofile(mccompat_modpath.."/blocklist.lua")
 
