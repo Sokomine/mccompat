f5ec3793432ddd0841bb6ab1d67dcb0ff728ac3f
Sokomine <wegwerf@anarres.dyndns.org>
2015-07-22 23:47:29 +0200

    moved xconnected to extra mod

--- init.lua
+++ init.lua
@@ -7,6 +7,5 @@ local mccompat_modpath = minetest.get_modpath( "mccompat");
 dofile(mccompat_modpath.."/gates.lua")
 dofile(mccompat_modpath.."/pressure_plates.lua")
 dofile(mccompat_modpath.."/liquids.lua")
-dofile(mccompat_modpath.."/xconnected.lua")
 dofile(mccompat_modpath.."/blocklist.lua")
 
